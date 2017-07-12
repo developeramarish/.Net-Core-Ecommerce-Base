7ce4e0d261c41058a0e6670bddc40ae061c8248e
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-12 09:11:23 +0100

    Constants

---
+++ src/Shop.Core/Constants/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop.Core.Constants
+{
+    public class Constants
+    {
+        public const int ReferenceLength = 10;
+        public const string DefaultDatabaseName = "Shop";
+        public const ulong MaxRequestAgeInSeconds = 300;
+        public const string ConnectionName = "ShopContext";
+    }
+}
