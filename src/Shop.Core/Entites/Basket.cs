e49a9ade0a4de5415f7c130fd70ecb8b50614be2
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-12 09:12:05 +0100

    Basket Entity

---
+++ src/Shop.Core/Entites/Basket.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Shop.Core.BaseObjects;
+using Shop.Core.Interfaces;
+
+namespace Shop.Core.Entites
+{
+    public class Basket : LifetimeBase, IReferenceable<Basket>
+    {
+
+        [Key]
+        public int BasketId { get; set; }
+        public string BasketReference { get; set; }
+        public List<ProductConfigurations> ProductConfigurations { get; set; }
+        public List<Products> Products { get; set; }
+        public ShippingMethod ShippingMethod { get; set; }
+        public DiscountCode DiscountCode { get; set; }
+
+        public Basket CreateReference(IReferenceGenerator referenceGenerator)
+        {
+            
+            BasketReference = referenceGenerator.CreateReference("B-", Constants.Constants.ReferenceLength);
+
+            return this;
+        }
+    }
+}
