7bd592d994f9e2cb8c1993c489fe0f20c3fc8d65
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-12 09:11:42 +0100

    CollectionExtensions

---
+++ src/Shop.Core/Extensions/CollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Shop.Core.Interfaces;
+
+namespace Shop.Core.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static IEnumerable<T> Active<T>(this ICollection<T> source) where T : ISoftDeletable
+        {
+            return source.Where(e => e.DateDeleted == null || e.DateDeleted > DateTime.UtcNow);
+        }
+    }
+}
