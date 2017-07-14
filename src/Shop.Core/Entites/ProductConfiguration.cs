0051c3acb1c25a08dabdc0c1c99196550d38cdfd
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-14 13:42:31 +0100

    ProductConfiguration

---
+++ src/Shop.Core/Entites/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Shop.Core.BaseObjects;
+using Shop.Core.Interfaces;
+
+namespace Shop.Core.Entites
+{
+    public class ProductConfiguration : LifetimeBase, IReferenceable<ProductConfiguration>
+    {
+        [Key]
+        public int ProductConfigurationId { get; set; }
+        public string ProductConfigurationReference { get; set; }
+
+        public Product BaseProduct { get; set; }
+        public List<ProductComponent> Components { get; set; }
+
+        public ProductConfiguration CreateReference(IReferenceGenerator referenceGenerator)
+        {
+            ProductConfigurationReference = referenceGenerator.CreateReference(Constants.Constants.ReferenceLength);
+            return this;
+        }
+    }
+}
