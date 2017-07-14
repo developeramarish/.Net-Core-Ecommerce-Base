2048d1479f447254bcd4e830413ecf7693e6374f
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-14 08:51:54 +0100

    shipping details entity

---
+++ src/Shop.Core/Entites/ShippingDetails.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Shop.Core.BaseObjects;
+using Shop.Core.Enums;
+using Shop.Core.Interfaces;
+
+namespace Shop.Core.Entites
+{
+    public class ShippingDetails : LifetimeBase, IReferenceable<ShippingDetails>
+    {
+        private ShippingProvider _shippingProvider;
+        private DateTime? _shippedDate;
+        private DateTime? _deliveredDate;
+        private DateTime? _estimatedDeliveryDate;
+        private DateTime? _estimatedShippingDeliveryDate;
+        private string _trackingReference;
+        private decimal _pricePaid;
+
+        [Key]
+        public int ShippingDetailsId { get; set; }
+        public string ShippingDetailsReference { get; set; }
+
+        public ShippingProvider ShippingProvider
+        {
+            get => _shippingProvider;
+            set
+            {
+                if (_shippingProvider == value) return;
+
+                IsDirty = true;
+                _shippingProvider = value;
+            }
+        }
+
+        public DateTime? EstimatedShippingDate
+        {
+            get => _estimatedShippingDeliveryDate;
+            set
+            {
+                if (_estimatedShippingDeliveryDate == value) return;
+
+                IsDirty = true;
+                _estimatedShippingDeliveryDate = value;
+            }
+        }
+        public DateTime? ShippedDate
+        {
+            get => _shippedDate;
+            set
+            {
+                if (_shippedDate == value) return;
+
+                IsDirty = true;
+                _shippedDate = value;
+            }
+        }
+
+        public DateTime? EstimatedDeliveryDate
+        {
+            get => _estimatedDeliveryDate;
+            set
+            {
+                if (_estimatedDeliveryDate == value) return;
+
+                IsDirty = true;
+                _estimatedDeliveryDate = value;
+            }
+        }
+        public DateTime? DeliveredDate
+        {
+            get => _deliveredDate;
+            set
+            {
+                if (_deliveredDate == value) return;
+
+                IsDirty = true;
+                _deliveredDate = value;
+            }
+        }
+
+        public decimal PricePaid
+        {
+            get => _pricePaid;
+            set
+            {
+                if (_pricePaid == value) return;
+
+                IsDirty = true;
+                _pricePaid = value;
+            }
+        }
+
+        public string Trackingreference
+        {
+            get => _trackingReference;
+            set
+            {
+                if (_trackingReference == value) return;
+
+                IsDirty = true;
+                _trackingReference = value;
+            }
+        }
+
+        public ShippingDetails CreateReference(IReferenceGenerator referenceGenerator)
+        {
+            ShippingDetailsReference = referenceGenerator.CreateReference("B-", Constants.Constants.ReferenceLength);
+            return this;
+        }
+    }
+}
