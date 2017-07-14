8f9beaedb179eccfc66a6a726c9143f2d316b57d
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-14 08:51:41 +0100

    Add discount code entity

---
+++ src/Shop.Core/Entites/DiscountCode.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Shop.Core.BaseObjects;
+using Shop.Core.Enums;
+using Shop.Core.Interfaces;
+
+namespace Shop.Core.Entites
+{
+    public class DiscountCode : LifetimeBase, IReferenceable<DiscountCode>
+    {
+        private string _code;
+        private DiscountCodeType _discountCodeType;
+        private decimal _discountAmount;
+        private int _claimLimit;
+        private int _numberClaimed;
+
+        [Key]
+        public int DiscountCodeId { get; set; }
+        public string DiscountCodeReference { get; set; }
+
+        public string Code
+        {
+            get => _code;
+            set
+            {
+                if (_code == value) return;
+
+                IsDirty = true;
+                _code = value;
+            }
+        }
+
+        public DiscountCodeType DiscountCodeType
+        {
+            get => _discountCodeType;
+            set
+            {
+                if (_discountCodeType == value) return;
+
+                IsDirty = true;
+                _discountCodeType = value;
+            }
+        }
+        public decimal DiscountAmount
+        {
+            get => _discountAmount;
+            set
+            {
+                if (_discountAmount == value) return;
+
+                IsDirty = true;
+                _discountAmount = value;
+            }
+        }
+        public int ClaimLimit
+        {
+            get => _claimLimit;
+            set
+            {
+                if (_claimLimit == value) return;
+
+                IsDirty = true;
+                _claimLimit = value;
+            }
+        }
+        public int NumberClaimed
+        {
+            get => _numberClaimed;
+            set
+            {
+                if (_numberClaimed == value) return;
+
+                IsDirty = true;
+                _numberClaimed = value;
+            }
+        }
+
+        public DiscountCode CreateReference(IReferenceGenerator referenceGenerator)
+        {
+            DiscountCodeReference = referenceGenerator.CreateReference("D-", Constants.Constants.ReferenceLength);
+            return this;
+        }
+    }
+}
