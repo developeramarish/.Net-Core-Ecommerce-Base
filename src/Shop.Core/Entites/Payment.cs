980e70df00a721722cb1e34450c211cbf790e7fb
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-14 11:44:17 +0100

    Payment Entity

---
+++ src/Shop.Core/Entites/Payment.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Shop.Core.BaseObjects;
+using Shop.Core.Enums;
+using Shop.Core.Interfaces;
+
+namespace Shop.Core.Entites
+{
+    public class Payment : LifetimeBase, IReferenceable<Payment>
+    {
+        private PaymentServiceProvider _paymentServiceProvider;
+        private string _pspChargeId;
+
+        [Key]
+        public int PaymentId { get; set; }
+        public string PaymentReference { get; set; }
+
+        public PaymentServiceProvider PaymentServiceProvider
+        {
+            get => _paymentServiceProvider;
+            set
+            {
+                if (_paymentServiceProvider == value) return;
+
+                IsDirty = true;
+                _paymentServiceProvider = value;
+            }
+        }
+        public string PspChargeId
+        {
+            get => _pspChargeId;
+            set
+            {
+                if (_pspChargeId == value) return;
+
+                IsDirty = true;
+                _pspChargeId = value;
+            }
+        }
+
+        public Payment CreateReference(IReferenceGenerator referenceGenerator)
+        {
+            PaymentReference = referenceGenerator.CreateReference("PAY-", Constants.Constants.ReferenceLength);
+            return this;
+        }
+    }
+}
---
+++ src/Shop.Core/Enums/PaymentServiceProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Shop.Core.Enums
+{
+    public enum PaymentServiceProvider
+    {
+        Unknown = 0,
+        Stripe = 1,
+        //etc,
+    }
+}
