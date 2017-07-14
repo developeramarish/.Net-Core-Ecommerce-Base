ff410638dba353ade55334274ff041eb53aebd89
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-14 09:09:02 +0100

    Add product component entity

---
+++ src/Shop.Core/Entites/ProductComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Shop.Core.BaseObjects;
+using Shop.Core.Interfaces;
+
+namespace Shop.Core.Entites
+{
+    public class ProductComponent : LifetimeBase, IReferenceable<ProductComponent>
+    {
+        [Key]
+        public int ProductComponentId { get; set; }
+        public string ProductComponentReference { get; set; }
+
+
+
+        public ProductComponent CreateReference(IReferenceGenerator referenceGenerator)
+        {
+            ProductComponentReference = referenceGenerator.CreateReference("PC-", Constants.Constants.ReferenceLength);
+            return this;
+        }
+    }
+}
