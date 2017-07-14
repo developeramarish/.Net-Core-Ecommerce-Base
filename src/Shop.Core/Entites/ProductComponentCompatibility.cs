65b563cf95bdeda0eb1631e33f22143866455dfd
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-14 09:04:13 +0100

    ProductComponentCompatibility entity

---
+++ src/Shop.Core/Entites/ProductComponentCompatibility.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Shop.Core.BaseObjects;
+using Shop.Core.Interfaces;
+
+namespace Shop.Core.Entites
+{
+    public class ProductComponentCompatibility : LifetimeBase, IReferenceable<Product>
+    {
+        [Key]
+        public int ProductComponentCompatibilityId { get; set; }
+        public string ProductComponentCompatibilityReference { get; set; }
+
+        public Product Product { get; set; }
+        public ProductComponent Component { get; set; }
+
+
+        public Product CreateReference(IReferenceGenerator referenceGenerator)
+        {
+            ProductComponentCompatibilityReference = referenceGenerator.CreateReference("P-", Constants.Constants.ReferenceLength);
+            return this;
+        }
+    }
+}
