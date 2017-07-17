5025d609ae4a13f10be4b6c4e0e7d90635a844c7
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-17 15:30:36 +0100

    AddressMapper

---
+++ src/Shop.Infrastructure.Data/Mappers/AddressMapper.cs
@@ -0,0 +1,26 @@
+﻿using Shop.Core.Entites;
+using Shop.Core.Interfaces;
+
+namespace Shop.Infrastructure.Data.Mappers
+{
+    public class AddressMapper : IMapToExisting<Address, Address>
+    {
+        public bool Map(Address source, Address target)
+        {
+            target.MarkClean();
+
+            target.AddressLine1 = source.AddressLine1;
+            target.AddressLine2 = source.AddressLine2;
+            target.AddressLine3 = source.AddressLine3;
+            target.AddressLine4 = source.AddressLine4;
+            target.Postcode = source.Postcode;
+            target.County = source.County;
+            target.CountryCode = source.CountryCode;
+
+            if (target.IsDirty)
+                target.MarkUpdated();
+
+            return target.IsDirty;
+        }
+    }
+}
