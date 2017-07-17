f14a687e5ed405afd704cc15b803bb09a7a3f23f
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-17 15:30:45 +0100

    BasketMapper

---
+++ src/Shop.Infrastructure.Data/Mappers/BasketMapper.cs
@@ -0,0 +1,20 @@
+﻿using Shop.Core.Entites;
+using Shop.Core.Interfaces;
+
+namespace Shop.Infrastructure.Data.Mappers
+{
+    public class BasketMapper : IMapToExisting<Basket, Basket>
+    {
+        public bool Map(Basket source, Basket target)
+        {
+            target.MarkClean();
+
+            target.ShippingMethod = source.ShippingMethod;
+
+            if (target.IsDirty)
+                target.MarkUpdated();
+
+            return target.IsDirty;
+        }
+    }
+}
