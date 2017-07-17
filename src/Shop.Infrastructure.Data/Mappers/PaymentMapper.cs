a97591aaff4e3b4d12915542252e260e0a0ecd7c
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-17 15:31:27 +0100

    PaymentMapper

---
+++ src/Shop.Infrastructure.Data/Mappers/PaymentMapper.cs
@@ -0,0 +1,21 @@
+﻿using Shop.Core.Entites;
+using Shop.Core.Interfaces;
+
+namespace Shop.Infrastructure.Data.Mappers
+{
+    public class PaymentMapper : IMapToExisting<Payment, Payment>
+    {
+        public bool Map(Payment source, Payment target)
+        {
+            target.MarkClean();
+
+            target.PaymentServiceProvider = source.PaymentServiceProvider;
+            target.PspChargeId = source.PspChargeId;
+
+            if (target.IsDirty)
+                target.MarkUpdated();
+
+            return target.IsDirty;
+        }
+    }
+}
