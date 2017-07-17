0320d2065c133e426d892f4f521a0f79b2a102ae
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-17 15:30:27 +0100

    QueryExtensions is active

---
+++ src/Shop.Infrastructure.Data/Extensions/QueryExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Shop.Core.Interfaces;
+
+namespace Shop.Infrastructure.Data.Extensions
+{
+    public static class QueryExtensions
+    {
+        public static IQueryable<T> Active<T>(this IQueryable<T> source) where T : class, ISoftDeletable
+        {
+            return source.Where(e => e.DateDeleted == null || e.DateDeleted > DateTime.UtcNow);
+        }
+    }
+}
