c7dc6e81c02bba9af5d6e9f2dc5c29642ee996cd
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-17 15:31:10 +0100

    MediaMapper

---
+++ src/Shop.Infrastructure.Data/Mappers/MediaMapper.cs
@@ -0,0 +1,22 @@
+﻿using Shop.Core.Entites;
+using Shop.Core.Interfaces;
+
+namespace Shop.Infrastructure.Data.Mappers
+{
+    public class MediaMapper : IMapToExisting<Media, Media>
+    {
+        public bool Map(Media source, Media target)
+        {
+            target.MarkClean();
+
+            target.MediaType = source.MediaType;
+            target.Url = source.Url;
+            target.Index = source.Index;
+
+            if (target.IsDirty)
+                target.MarkUpdated();
+
+            return target.IsDirty;
+        }
+    }
+}
