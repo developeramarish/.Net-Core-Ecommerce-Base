de1f4d07d067e0a3a840db2e490ea761b34b16c9
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-17 15:31:35 +0100

    ProductMapper

---
+++ src/Shop.Infrastructure.Data/Mappers/ProductMapper.cs
@@ -0,0 +1,26 @@
+﻿using Shop.Core.Entites;
+using Shop.Core.Interfaces;
+
+namespace Shop.Infrastructure.Data.Mappers
+{
+    public class ProductMapper : IMapToExisting<Product, Product>
+    {
+        public bool Map(Product source, Product target)
+        {
+            target.MarkClean();
+
+            target.ProductName = source.ProductName;
+            target.ProductShortDescription = source.ProductShortDescription;
+            target.ProductDescription = source.ProductDescription;
+            target.PricePreTax = source.PricePreTax;
+            target.ShippingWeightKg = source.ShippingWeightKg;
+            target.AvailableForOrder = source.AvailableForOrder;
+            target.Configureable = source.Configureable;
+
+            if (target.IsDirty)
+                target.MarkUpdated();
+
+            return target.IsDirty;
+        }
+    }
+}
