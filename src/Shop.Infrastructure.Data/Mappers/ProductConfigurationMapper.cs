c74956b70fa42f6d1ddd43a1e68599486541d891
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-17 16:07:33 +0100

    ProductConfigurationMapper

---
+++ src/Shop.Infrastructure.Data/Mappers/ProductConfigurationMapper.cs
@@ -0,0 +1,20 @@
+﻿using Shop.Core.Entites;
+using Shop.Core.Interfaces;
+
+namespace Shop.Infrastructure.Data.Mappers
+{
+    public class ProductConfigurationMapper: IMapToExisting<ProductConfiguration, ProductConfiguration>
+    {
+        public bool Map(ProductConfiguration source, ProductConfiguration target)
+        {
+            target.MarkClean();
+
+            //No components mapped at this time
+
+            if (target.IsDirty)
+                target.MarkUpdated();
+
+            return target.IsDirty;
+        }
+    }
+}
