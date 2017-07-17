3154d8da26cfa3f3ce98538a6096e6b52adc8c39
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-17 15:55:24 +0100

    ProductComponentMapper

---
+++ src/Shop.Infrastructure.Data/Mappers/ProductComponentCompatibilityMapper.cs
@@ -0,0 +1,20 @@
+﻿using Shop.Core.Entites;
+using Shop.Core.Interfaces;
+
+namespace Shop.Infrastructure.Data.Mappers
+{
+    public class ProductComponentCompatibilityMapper : IMapToExisting<ProductComponentCompatibility, ProductComponentCompatibility>
+    {
+        public bool Map(ProductComponentCompatibility source, ProductComponentCompatibility target)
+        {
+            target.MarkClean();
+
+            //No components mapped at this time
+
+            if (target.IsDirty)
+                target.MarkUpdated();
+
+            return target.IsDirty;
+        }
+    }
+}
---
+++ src/Shop.Infrastructure.Data/Mappers/ProductComponentMapper.cs
@@ -0,0 +1,25 @@
+﻿using Shop.Core.Entites;
+using Shop.Core.Interfaces;
+
+namespace Shop.Infrastructure.Data.Mappers
+{
+    public class ProductComponentMapper : IMapToExisting<ProductComponent, ProductComponent>
+    {
+        public bool Map(ProductComponent source, ProductComponent target)
+        {
+            target.MarkClean();
+
+            target.ProductComponentName = source.ProductComponentName;
+            target.ProductComponentDescription = source.ProductComponentDescription;
+            target.ComponentSlot = source.ComponentSlot;
+            target.PricePreTax = source.PricePreTax;
+            target.AvailableForOrder = source.AvailableForOrder;
+            target.ShippingWeightKg = source.ShippingWeightKg;
+
+            if (target.IsDirty)
+                target.MarkUpdated();
+
+            return target.IsDirty;
+        }
+    }
+}
