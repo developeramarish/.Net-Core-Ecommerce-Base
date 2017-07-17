868832d9e2a59b528e2e8d466237911bef74c8d3
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-17 14:54:10 +0100

    ignore IsDirty in db

--- src/Shop.Infrastructure.Data/ShopContext.cs
+++ src/Shop.Infrastructure.Data/ShopContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Shop.Core.Constants;
 using Shop.Core.Entites;
 
 namespace Shop.Infrastructure.Data
@@ -24,5 +23,23 @@ namespace Shop.Infrastructure.Data
         public DbSet<ProductComponentCompatibility> ProductComponentCompatibilities { get; set; }
         public DbSet<ProductConfiguration> ProductConfigurations { get; set; }
         public DbSet<ShippingDetails> ShippingDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //Ensure the IsDirty check (LifetimeBase) is not mapped to the db
+            modelBuilder.Entity<Address>().Ignore(x => x.IsDirty);
+            modelBuilder.Entity<Basket>().Ignore(x => x.IsDirty);
+            modelBuilder.Entity<Customer>().Ignore(x => x.IsDirty);
+            modelBuilder.Entity<DiscountCode>().Ignore(x => x.IsDirty);
+            modelBuilder.Entity<Media>().Ignore(x => x.IsDirty);
+            modelBuilder.Entity<Order>().Ignore(x => x.IsDirty);
+            modelBuilder.Entity<Payment>().Ignore(x => x.IsDirty);
+            modelBuilder.Entity<Product>().Ignore(x => x.IsDirty);
+            modelBuilder.Entity<ProductComponent>().Ignore(x => x.IsDirty);
+            modelBuilder.Entity<ProductComponentCompatibility>().Ignore(x => x.IsDirty);
+            modelBuilder.Entity<ProductConfiguration>().Ignore(x => x.IsDirty);
+            modelBuilder.Entity<ShippingDetails>().Ignore(x => x.IsDirty);
+        }
     }
 }
