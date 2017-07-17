d25f8d6bd8eac6a773428c9c1143edb12b705749
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-17 15:31:00 +0100

    DiscountCodeMapper

---
+++ src/Shop.Infrastructure.Data/Mappers/DiscountCodeMapper.cs
@@ -0,0 +1,24 @@
+﻿using Shop.Core.Entites;
+using Shop.Core.Interfaces;
+
+namespace Shop.Infrastructure.Data.Mappers
+{
+    public class DiscountCodeMapper : IMapToExisting<DiscountCode, DiscountCode>
+    {
+        public bool Map(DiscountCode source, DiscountCode target)
+        {
+            target.MarkClean();
+
+            target.Code = source.Code;
+            target.DiscountCodeType = source.DiscountCodeType;
+            target.DiscountAmount = source.DiscountAmount;
+            target.ClaimLimit = source.ClaimLimit;
+            target.NumberClaimed = source.NumberClaimed;
+
+            if (target.IsDirty)
+                target.MarkUpdated();
+
+            return target.IsDirty;
+        }
+    }
+}
