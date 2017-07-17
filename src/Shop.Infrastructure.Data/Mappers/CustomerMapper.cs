52b313e96c97abc0a168acccddc51bfd46d76b61
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-17 15:30:51 +0100

    CustomerMapper

---
+++ src/Shop.Infrastructure.Data/Mappers/CustomerMapper.cs
@@ -0,0 +1,22 @@
+﻿using Shop.Core.Entites;
+using Shop.Core.Interfaces;
+
+namespace Shop.Infrastructure.Data.Mappers
+{
+    public class CustomerMapper : IMapToExisting<Customer, Customer>
+    {
+        public bool Map(Customer source, Customer target)
+        {
+            target.MarkClean();
+            target.Title = source.Title;
+            target.Name = source.Name;
+            target.EmailAddress = source.EmailAddress;
+            target.TelephoneNumber = source.TelephoneNumber;
+
+            if (target.IsDirty)
+                target.MarkUpdated();
+
+            return target.IsDirty;
+        }
+    }
+}
