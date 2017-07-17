ba14c374e6dd1fcac7e9cd94bdb6920de34a24a9
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-17 15:31:17 +0100

    OrderMapper

---
+++ src/Shop.Infrastructure.Data/Mappers/OrderMapper.cs
@@ -0,0 +1,20 @@
+﻿using Shop.Core.Entites;
+using Shop.Core.Interfaces;
+
+namespace Shop.Infrastructure.Data.Mappers
+{
+    public class OrderMapper : IMapToExisting<Order, Order>
+    {
+        public bool Map(Order source, Order target)
+        {
+            target.MarkClean();
+
+            //No components mapped at this time
+
+            if (target.IsDirty)
+                target.MarkUpdated();
+
+            return target.IsDirty;
+        }
+    }
+}
