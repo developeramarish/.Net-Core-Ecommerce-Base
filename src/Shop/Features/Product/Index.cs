10852b41f9b924a5230cc0d06786abb0787e35ce
Jon Tansey <jonathan.tansey@googlemail.com>
2017-07-19 12:22:31 +0100

    Add product index feature

---
+++ src/Shop/Features/Product/Index.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace Shop.Features.Product
+{
+    public class Index
+    {
+        public class Query : IRequest<Model>
+        {
+        }
+    }
+
+    public class Model
+    {
+        public List<Product> Products { get; set; }
+
+        public class Product
+        {
+            public string ProductReference { get; set; }
+            public string ProductName { get; set; }
+            public string ProductShortDescription { get; set; }
+            public string Price { get; set; }
+
+            public Media CoverImage { get; set; }
+        }
+
+        public class Media
+        {
+            public string Url { get; set; }
+        }
+    }
+}
